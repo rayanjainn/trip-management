61cffaa682920f187be1a0c16699b22c282048a6
Rayan Jain <rayanjain@Ray-2.local>
2024-11-28 14:27:05 +0530

    update vite config & rm App.css

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config({ path: "../../.env" });
 
 // vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
-    define: {
-      "process.env.DB_ADMIN_USERNAME": JSON.stringify(
-        process.env.DB_ADMIN_USERNAME
-      ),
-      "process.env.DB_ADMIN_PASSWORD": JSON.stringify(
-        process.env.DB_ADMIN_PASSWORD
-      ),
-    },
     plugins: [react()],
   };
 });
